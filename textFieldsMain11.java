47eb59142ef175c1c2c4a9318c1ebea4db9356f0
AnkitindiiitA <97433903+AnkitindiiitA@users.noreply.github.com>
2022-11-07 19:26:43 +0530

    Create textFieldsMain11.java

---
+++ textFieldsMain11.java
@@ -0,0 +1,9 @@
+public class textFieldsMain11 {
+    public static void main(String[] args) {
+
+        //JTextField = a GUI textbox component that can be used to add, set, or get text
+
+        textFields11 frame = new textFields11();
+
+    }
+}
